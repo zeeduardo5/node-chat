7e725aeceaa06af0d1a759fe670fd011a035daec
José Assunção - MindSource <jose.assuncao@mindsource.pt>
2018-10-15 16:49:01 +0100

    add form

--- server/server.js
+++ server/server.js
@@ -19,12 +19,11 @@ io.on('connection', (socket) => {
 
     socket.broadcast.emit('newMessage', generateMessage('Admin','New User joined'));
 
-    socket.on('createMessage', (message) => {
+    socket.on('createMessage', (message, callBack) => {
         console.log('createMessage', message);
 
-
         io.emit('newMessage', generateMessage(message.from,message.text));
-
+        callBack('This is from the server');
         // socket.broadcast.emit('newMessage', {
         //     from: message.from,
         //     text: message.text,
