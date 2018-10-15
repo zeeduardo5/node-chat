f22fcb4c95dc642bb1a6d050a6e70469d0f84bc2
José Assunção - MindSource <jose.assuncao@mindsource.pt>
2018-10-15 16:02:31 +0100

    create generate message utility

--- server/server.js
+++ server/server.js
@@ -1,12 +1,42 @@
 const path = require('path');
+const http = require('http');
 const express = require('express');
+const socketIO = require('socket.io');
 
+const {generateMessage} = require('./utils/message');
 const publicPath = path.join(__dirname, '../public');
 const port = process.env.PORT || 3000;
 var app = express();
+var server = http.createServer(app);
+var io = socketIO(server);
 
 app.use(express.static(publicPath));
 
-app.listen(port, () => {
+io.on('connection', (socket) => {
+    console.log('New user connected');
+
+    socket.emit('newMessage', generateMessage('Admin','Welcome to the chat app'));
+
+    socket.broadcast.emit('newMessage', generateMessage('Admin','New User joined'));
+
+    socket.on('createMessage', (message) => {
+        console.log('createMessage', message);
+
+
+        io.emit('newMessage', generateMessage(message.from,message.text));
+
+        // socket.broadcast.emit('newMessage', {
+        //     from: message.from,
+        //     text: message.text,
+        //     createdAt: new Date().getTime()
+        // })
+    })
+
+    socket.on('disconnect', () => {
+        console.log('User was disconnected from server');
+    });
+})
+
+server.listen(port, () => {
     console.log(`Server is up on ${port}`);
 })
---
+++ server/utils/message.test.js
@@ -0,0 +1,17 @@
+var expect = require('expect');
+
+var {generateMessage} = require('./message');
+
+describe('generateMessage', ()=>{
+    it('should generate correct message object', () => {
+        var from = 'john';
+        var text = 'Some message';
+        var message = generateMessage(from, text);
+        
+        expect(message.createAt).toBeA('number');
+        expect(message).toInclude({
+            from,
+            text
+        })
+    })
+})
