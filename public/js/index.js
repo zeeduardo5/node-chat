6430f83c656383a4b3ad837d01c0869d2676fe34
José Assunção - MindSource <jose.assuncao@mindsource.pt>
2018-10-16 12:10:23 +0100

    added scroll

--- public/js/index.js
+++ public/js/index.js
@@ -1,5 +1,21 @@
 var socket = io();
 
+function scrollToBottom() {
+    // selectors
+    var messages = $('#messages');
+    var newMessage = messages.children('li:last-child');
+    // height
+    var clientHeight = messages.prop('clientHeight');
+    var scrollTop = messages.prop('scrollTop');
+    var scrollHeight = messages.prop('scrollHeight');
+    var newMessageHeight = newMessage.innerHeight();
+    var lastMessageHeigth = newMessage.prev().innerHeight();
+
+    if (clientHeight + scrollTop + newMessageHeight + lastMessageHeigth >= scrollHeight) {
+        messages.scrollTop(scrollHeight);
+    }
+}
+
 socket.on('connect', function () {
     console.log('Connected to server');
 });
@@ -10,19 +26,26 @@ socket.on('disconnect', function () {
 
 socket.on('newMessage', function (message) {
     var formattedTime = moment(message.createAt).format('h:mm a')
-    var li = $('<li></li>');
-    li.text(message.from + ' '+ formattedTime +': ' + message.text  );
-    $('#messages').append(li);
+    var template = $('#message-template').html();
+    var html = Mustache.render(template, {
+        text: message.text,
+        from: message.from,
+        createAt: formattedTime
+    });
+    $('#messages').append(html);
+    scrollToBottom();
 })
 
 socket.on('newLocationMessage', function (message) {
-    console.log('newLocationMessage', message);
-    var li = $('<li></li>');
-    var a = $('<a target="_blank">My current Location</a>')
-    li.text(message.from + ' '+ formattedTime +': ');
-    a.attr('href', message.url);
-    li.append(a);
-    $('#messages').append(li);
+    var formattedTime = moment(message.createAt).format('h:mm a');
+    var template = $('#location-message-template').html();
+    var html = Mustache.render(template, {
+        url: message.url,
+        from: message.from,
+        createAt: formattedTime
+    });
+    $('#messages').append(html);
+    scrollToBottom();
 });
 
 var messageTextbox = $('[name=message]');
