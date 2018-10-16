764ea8da80aa08feb2ab83cd211459ca550e6398
José Assunção - MindSource <jose.assuncao@mindsource.pt>
2018-10-16 11:25:26 +0100

    added timestamp

--- public/js/index.js
+++ public/js/index.js
@@ -9,9 +9,9 @@ socket.on('disconnect', function () {
 });
 
 socket.on('newMessage', function (message) {
-    console.log('newMessage', message);
+    var formattedTime = moment(message.createAt).format('h:mm a')
     var li = $('<li></li>');
-    li.text(message.from + ': ' + message.text);
+    li.text(message.from + ' '+ formattedTime +': ' + message.text  );
     $('#messages').append(li);
 })
 
@@ -19,7 +19,7 @@ socket.on('newLocationMessage', function (message) {
     console.log('newLocationMessage', message);
     var li = $('<li></li>');
     var a = $('<a target="_blank">My current Location</a>')
-    li.text(message.from + ': ');
+    li.text(message.from + ' '+ formattedTime +': ');
     a.attr('href', message.url);
     li.append(a);
     $('#messages').append(li);
