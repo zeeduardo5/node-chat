762ab1fbc536e726c34be846d6614a26df1d27df
José Assunção - MindSource <jose.assuncao@mindsource.pt>
2018-10-16 10:33:11 +0100

    added css and ux

--- public/js/index.js
+++ public/js/index.js
@@ -19,20 +19,21 @@ socket.on('newLocationMessage', function (message) {
     console.log('newLocationMessage', message);
     var li = $('<li></li>');
     var a = $('<a target="_blank">My current Location</a>')
-    li.text(message.from + ': ' );
+    li.text(message.from + ': ');
     a.attr('href', message.url);
     li.append(a);
     $('#messages').append(li);
 });
 
+var messageTextbox = $('[name=message]');
 $('#message-form').on('submit', function (e) {
     e.preventDefault();
 
     socket.emit('createMessage', {
         from: 'User',
-        text: $('[name=message]').val()
-    }, function (data) {
-        console.log(data);
+        text: messageTextbox.val()
+    }, function () {
+        messageTextbox.val('')
     });
 })
 
@@ -42,7 +43,10 @@ locationButton.on('click', function ($event) {
         return alert('Geolocation not supported by your browser');
     }
 
+    locationButton.attr('disabled', 'disabled').text('Sending Location ...');
+
     navigator.geolocation.getCurrentPosition(function (position) {
+        locationButton.removeAttr('disabled').text('Send Location');
         socket.emit('createLocationMessage', {
             latitude: position.coords.latitude,
             longitude: position.coords.longitude
@@ -50,6 +54,7 @@ locationButton.on('click', function ($event) {
             console.log(data);
         });
     }, function () {
+        locationButton.removeAttr('disabled').text('Send Location');
         alert('Geolocation not fetched');
     })
 })
